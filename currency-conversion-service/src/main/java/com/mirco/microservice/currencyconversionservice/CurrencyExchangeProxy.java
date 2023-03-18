7bfba79930a32a962f1a0976c9cd0b50704b4607
Qiming Ye <1281784448yeqiming@gmail.com>
2023-03-18 18:12:03 -0400

    Spring boot microservices with docker-compose

---
+++ currency-conversion-service/src/main/java/com/mirco/microservice/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.mirco.microservice.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from,
+                                                    @PathVariable String to);
+}
---
+++ currency-exachange-service/src/main/java/com/micro/microservices/currencyexachangeservice/CurrencyExachangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.microservices.currencyexachangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExachangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExachangeServiceApplication.class, args);
+	}
+
+}
